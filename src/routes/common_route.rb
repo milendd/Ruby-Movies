5ab6fdb5bbdc29049dfbbc46fefe47fa27e20dee
Milen Donchev <milen_d@abv.bg>
2017-08-30 01:09:22 +0300

    Add styles, bootstrap, search form

--- src/routes/common_route.rb
+++ src/routes/common_route.rb
@@ -3,8 +3,17 @@ require 'sinatra/base'
 module Sinatra
   module CommonRoute
     def self.registered(app)
+      
       app.get '/' do
         erb :'./home/index'
+      end
+      
+      app.get '/search' do
+        # TODO:
+        @query = params[:query]
+        @movies = []
+        @people = []
+        erb :'./home/search'
       end
     end
   end
