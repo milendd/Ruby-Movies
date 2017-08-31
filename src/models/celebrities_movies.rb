8bba8cba2f18cacf1e84e8f7748a074cd2f86aea
Milen Donchev <milen_d@abv.bg>
2017-09-01 01:11:54 +0300

    Add details view for movie

--- src/models/celebrities_movies.rb
+++ src/models/celebrities_movies.rb
@@ -1,2 +1,5 @@
 class CelebritiesMovies < ActiveRecord::Base
+  belongs_to :movie
+  belongs_to :celebrity
+  belongs_to :celebrity_type
 end
--- src/routes/movies_route.rb
+++ src/routes/movies_route.rb
@@ -1,11 +1,17 @@
 require 'sinatra/base'
 
 require_relative '../models/movie'
+require_relative '../models/celebrities_movies'
+require_relative '../models/celebrity_type'
 
 module Sinatra
   module MoviesRoute
+    def self.init
+      @actor_type_id = CelebrityType.where(name: 'Actor').first.id
+      @director_type_id = CelebrityType.where(name: 'Director').first.id
+    end
+    
     def self.registered(app)
-      
       app.get '/movies' do
         # todo: page size and page count
         @movies = Movie.all
@@ -13,7 +19,11 @@ module Sinatra
       end
       
       app.get '/movies/:id' do
-        @movie = Movie.where(id: params[:id]).first
+        MoviesRoute.init
+        @all_data = CelebritiesMovies.where(movies_id: params[:id])
+        if @all_data.any?
+          @movie = Movie.where(id: params[:id]).first
+        end
         erb :'./movies/view'
       end
     end
