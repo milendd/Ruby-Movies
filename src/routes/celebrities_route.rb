59a465452ee0cbcd338ca93e75c2260988835062
Milen Donchev <milen_d@abv.bg>
2017-09-02 01:36:27 +0300

    Visualize movies for selected celebrity, also extract error message

--- src/routes/celebrities_route.rb
+++ src/routes/celebrities_route.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 
 require_relative '../models/celebrity'
+require_relative '../models/celebrities_movie'
 
 module Sinatra
   module CelebritiesRoute
@@ -14,6 +15,7 @@ module Sinatra
       
       app.get '/celebrities/:id' do
         @celebrity = Celebrity.where(id: params[:id]).first
+        @all_data = CelebritiesMovie.where(celebrity_id: params[:id])
         erb :'./celebrities/view'
       end
     end
